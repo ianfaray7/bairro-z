672e294afeaec9243ea9f629483c100154f4ce39
unknown <lucasfpp@al.insper.edu.br>
2025-11-20 14:10:30 -0900

    reiniciar, menu, continuar, end e esc

---
+++ AssetsFilizola/_Scripts/PauseController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
